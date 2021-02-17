ad3a375125306c8b14feb50cacb1e5b70cd948c8
Eike Kirch <eikekirch@icloud.com>
2021-02-17 20:14:55 +0100

    Working mvp, polished state

---
+++ config.py
@@ -0,0 +1,19 @@
+"""
+    Edit this file to enable this application on your grade management system.
+"""""
+
+WEBSITE_URI         = 'https://icms.hs-hannover.de'
+USERNAME_INPUT_ID   = 'asdf'
+PASSWORD_INPUT_ID   = 'fdsa'
+
+NAVIGATION_XPATHS   = [
+    '//*[@id="makronavigation"]/ul/li[3]/a',
+    '//*[@id="wrapper"]/div[6]/div[2]/div/form/div/ul/li[2]/a',
+    '//*[@id="wrapper"]/div[6]/div[2]/form/ul/li/a[2]',
+]
+
+CONTENT_ELEM_ID     = 'wrapper'
+
+LOGOUT_XPATH        = '//*[@id="wrapper"]/div[4]/div/a'
+
+INTERVAL            = 10
